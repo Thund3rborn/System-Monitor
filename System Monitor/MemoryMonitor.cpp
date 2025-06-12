77504cf88a4a83f70c42b380b9768c146a94decf
Thund3rborn <kacper.feister@outlook.com>
2025-06-12 21:00:47 +0200

    Implemented the MemoryMonitor .cpp class

--- System Monitor/MemoryMonitor.cpp
+++ System Monitor/MemoryMonitor.cpp
@@ -5,5 +5,28 @@
 
 float MemoryMonitor::getUsage()
 {
+	std::ifstream file("/proc/meminfo");
+	std::string label;
+	long memoryTotal = 0, memoryAvailable = 0;
 
+	//reads one word at a time from the "file" into "label" until end of the file
+	while (file >> label)
+	{
+		if (label == "MemoryTotal:")
+		{
+			file >> memoryTotal;
+		}
+		else if (label == "MemoryAvailable:")
+		{
+			file >> memoryAvailable;
+			break;
+		}
+		else
+		{
+			file.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip line
+		}
+	}
+
+	if (memoryTotal == 0) return 0.0f;
+	return 100.0f * (1.0f - static_cast<float>(memoryAvailable) / memoryTotal); //calculate usage
 }
